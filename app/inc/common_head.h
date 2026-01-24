7f3b83a3d8908f6b69b291851eff8c5874fa78ad
Brunthx <a15992465412@163.com>
2026-01-24 18:18:44 +0800

    [sys_monitor] update common_head.h by Edison

--- app/inc/common_head.h
+++ app/inc/common_head.h
@@ -15,4 +15,14 @@
 #include "../../libmslog/inc/mslog.h"
 #include "../../libmslog/inc/mslog_thread.h"
 
+//type define
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
 #endif//__COMMON_HEAD_H__
